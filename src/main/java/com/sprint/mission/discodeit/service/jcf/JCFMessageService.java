715fb5f5583cde54ef889e3504079ac844bbbb66
kmg-git <kmg771kmg@gmail.com>
2026-08-18 11:47:28 +0900

    JCFMessageService 작업 오전마무리

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -1,4 +1,45 @@
 package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.Reaction;
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
 //반응 숫자 맵
 public class JCFMessageService implements MessageService{
+    final Map<UUID, Message>  messageMap = new HashMap<>();
+    final Map<UUID ,Map<Reaction, Set<User>>> reactionMap = new HashMap<>();
+    //2nf?
+    private final static JCFMessageService instance = new JCFMessageService();
+
+    private JCFMessageService() { }
+
+    public static JCFMessageService getInstance() {
+        return instance;
+    }
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
 }
--- src/main/java/com/sprint/mission/discodeit/service/jcf/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/MessageService.java
@@ -1,4 +1,12 @@
 package com.sprint.mission.discodeit.service.jcf;
 
+import com.sprint.mission.discodeit.entity.NitroLevel;
+
+import java.util.UUID;
+
 public interface MessageService {
+    void create();
+    void read();
+    void update();
+    void delete();
 }
