6f615e25d1febc7c249e0d0a7c0587229b6e4c7e
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2026-08-25 14:48:07 +0900

    JCFChannelRepository 작성

--- src/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
+++ src/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
@@ -1,4 +1,44 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
-public class JCFChannelRepository {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+
+import java.util.*;
+
+public class JCFChannelRepository implements ChannelRepository {
+    private final Map<UUID, Channel> data = new HashMap<>();
+
+    @Override
+    public Channel save(Channel channel)
+    {
+        data.put(channel.getId(), channel);
+        return channel;
+    }
+
+    @Override
+    public Optional<Channel> findById(UUID id)
+    {
+        return Optional.ofNullable(data.get(id));
+    }
+    @Override
+    public List<Channel> findAll()
+    {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void deleteById(UUID id)
+    {
+        data.remove(id);
+    }
+
+    @Override
+    public boolean existById(UUID id)
+    {
+        return data.containsKey(id);
+    }
+
+
+
+
 }
