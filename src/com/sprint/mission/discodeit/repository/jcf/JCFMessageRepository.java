4931176b49a712f4a1868a5f14d6b182c79c318f
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2026-08-25 17:15:02 +0900

    JCFMessageRepository 작성

--- src/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
+++ src/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -1,4 +1,51 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
-public class JCFMessageRepository {
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.MessageService;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class JCFMessageRepository implements MessageRepository {
+
+    private final Map<UUID, Message> data = new HashMap<>();
+
+    @Override
+    public Message save(Message message)
+    {
+        data.put(message.getId(), message);
+        return message;
+    }
+
+    @Override
+    public Optional<Message> findById(UUID id)
+    {
+        return Optional.ofNullable(data.get(id));
+    }
+    @Override
+    public List<Message> findAllByChannelId(UUID channelId)
+    {
+        return data.values().stream()
+                .filter(message -> message.getChannelId().equals(channelId))
+                .collect(Collectors.toList());
+    }
+
+
+    @Override
+    public List<Message> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void deleteById(UUID id)
+    {
+        data.remove(id);
+    }
+
+    @Override
+    public boolean existById(UUID id)
+    {
+        return data.containsKey(id);
+    }
 }
