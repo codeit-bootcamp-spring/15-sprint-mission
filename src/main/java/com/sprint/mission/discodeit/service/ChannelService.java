cd059d20de7b852f942f0ab26315b64307171626
준영 <gksrmf1210@gmail.com>
2026-09-01 17:56:52 +0900

    ChannelService 생성

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -3,15 +3,17 @@ package com.sprint.mission.discodeit.service;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
 
-import java.awt.*;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ChannelService {
 
     Channel create(Channel channel);
 
-    Channel findById(UUID id);
+    Channel create(ChannelType type, String name, String description);
+
+    Optional<Channel> findById(UUID id);
 
     List<Channel> findAll();
 
