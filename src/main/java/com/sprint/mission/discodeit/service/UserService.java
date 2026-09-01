8fa1281622dfd52870d11454a762c181e4c57ac1
준영 <gksrmf1210@gmail.com>
2026-09-01 17:56:19 +0900

    UserService 수정

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -2,19 +2,25 @@ package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.entity.User;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
 
     User create(User user);
 
-    User findById(UUID id);
+    User create(String username, String email, String password) throws IOException;
+
+    Optional<User> findById(UUID id);
 
     List<User> findAll();
 
-    User update(UUID id, String name);
+    Optional<Object> update(UUID id, String name);
 
-    void delete(UUID id);
+    void delete(UUID id) throws IOException;
+
+    List<User> findall();
 }
 
