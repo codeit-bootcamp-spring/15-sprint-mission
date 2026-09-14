4f460b188182a228cee365a3440fae8a210791a7
준영 <gksrmf1210@gmail.com>
2026-09-14 10:44:53 +0900

    feat: 상태 UserStatusRepository 추가

---
+++ src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.UserStatus;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserStatusRepository {
+
+
+    //저장
+    UserStatus save(UserStatus userStatus);
+    // 상태 ID로 찾기
+    Optional<UserStatus> findById(UUID id);
+    // findAll() 전체 상태 조회
+    List<UserStatus> findAll();
+    // findByUserId() 특정 사용자의 상태 조회
+    Optional<UserStatus> findByUserId(UUID userId);
+    // 삭제
+    void delete(UUID id);
+
+
+
+}
