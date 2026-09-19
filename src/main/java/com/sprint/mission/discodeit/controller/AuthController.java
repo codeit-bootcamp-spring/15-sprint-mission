7af415a4b31a88d189a070d88e4a1e1084528475
최태민 <ctm0522@gmail.com>
2026-09-19 22:44:58 +0900

    [최태민] sprint4_RequestMapping으로 요구사항에 맞춰 리팩터링2

---
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.common.ApiResponse;
+import com.sprint.mission.discodeit.dto.request.LoginRequest;
+import com.sprint.mission.discodeit.dto.response.LoginResponse;
+import com.sprint.mission.discodeit.dto.response.UserResponse;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.basic.BasicAuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/login")
+public class AuthController {
+    private final BasicAuthService authService;
+
+    public AuthController(BasicAuthService authService) {
+        this.authService = authService;
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public ResponseEntity<ApiResponse<LoginResponse>> createLogin(
+            @Valid @ModelAttribute LoginRequest loginRequest
+            ) {
+        User user = authService.login(loginRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(LoginResponse.from(user)));
+    }
+
+}
