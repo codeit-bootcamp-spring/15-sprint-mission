05e7a7f523ab01a4b3234e27f202e741929713eb
최태민 <ctm0522@gmail.com>
2026-09-20 18:35:36 +0900

    [최태민] sprint4_웹 API 예외 전역 처리 GlobalExceptionHandler 구현 완료

--- src/main/java/com/sprint/mission/discodeit/common/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/common/GlobalExceptionHandler.java
@@ -16,37 +16,37 @@ public class GlobalExceptionHandler {
     // NoSuchArguments -> NotFoundException
     @ExceptionHandler(BinaryContentNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidation(BinaryContentNotFoundException e) {
-        ApiError error = new ApiError("ILLEGAL_ARGUMENT", e.getMessage());
+        ApiError error = new ApiError("BINARY_CONTENT_NOT_FOUND", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(error));
     }
     @ExceptionHandler(ChannelNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidation(ChannelNotFoundException e) {
-        ApiError error = new ApiError("ILLEGAL_ARGUMENT", e.getMessage());
+        ApiError error = new ApiError("CHANNEL_NOT_FOUND", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(error));
     }
     @ExceptionHandler(MessageNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidation(MessageNotFoundException e) {
-        ApiError error = new ApiError("ILLEGAL_ARGUMENT", e.getMessage());
+        ApiError error = new ApiError("MESSAGE_NOT_FOUND", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(error));
     }
     @ExceptionHandler(ReadStatusNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidation(ReadStatusNotFoundException e) {
-        ApiError error = new ApiError("ILLEGAL_ARGUMENT", e.getMessage());
+        ApiError error = new ApiError("READ_STATUS_NOT_FOUND", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(error));
     }
     @ExceptionHandler(UserNameNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidation(UserNameNotFoundException e) {
-        ApiError error = new ApiError("ILLEGAL_ARGUMENT", e.getMessage());
+        ApiError error = new ApiError("USERNAME_NOT_FOUND", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(error));
     }
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidation(UserNotFoundException e) {
-        ApiError error = new ApiError("ILLEGAL_ARGUMENT", e.getMessage());
+        ApiError error = new ApiError("USER_NOT_FOUND", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(error));
     }
     @ExceptionHandler(UserStatusNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleValidation(UserStatusNotFoundException e) {
-        ApiError error = new ApiError("ILLEGAL_ARGUMENT", e.getMessage());
+        ApiError error = new ApiError("USER_STATUS_NOT_FOUND", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(error));
     }
 
