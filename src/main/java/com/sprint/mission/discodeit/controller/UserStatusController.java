1b3c1b83dcfa3dd52f5d7f43b25d0191b20967ed
최태민 <ctm0522@gmail.com>
2026-09-21 16:05:14 +0900

    [최태민] sprint4

--- src/main/java/com/sprint/mission/discodeit/controller/UserStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserStatusController.java
@@ -1,24 +1,15 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.common.ApiResponse;
-import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
-import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
-import com.sprint.mission.discodeit.dto.request.UserStatusCreateRequest;
+
 import com.sprint.mission.discodeit.dto.request.UserStatusUpdateRequest;
-import com.sprint.mission.discodeit.dto.response.UserResponse;
 import com.sprint.mission.discodeit.dto.response.UserStatusResponse;
-import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
-import com.sprint.mission.discodeit.repository.UserStatusRepository;
-import com.sprint.mission.discodeit.service.UserStatusService;
 import com.sprint.mission.discodeit.service.basic.BasicUserStatusService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
